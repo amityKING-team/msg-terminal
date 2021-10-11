db3188e9d832135bcfedef8844ffe5f3a8bfd18f
whoisprivalov <nikita.karatsev@gmail.com>
2021-10-11 23:40:17 +0600

    Add Rust branch

--- src/tui/component/border.cpp
+++
@@ -1,7 +0,0 @@
-#include <msg_terminal.hpp>
-
-void BorderComponent::setHaveBorder(bool haveBorder) {
-  _haveBorder = haveBorder;
-}
-
-bool BorderComponent::getHaveBorder() { return _haveBorder; }
