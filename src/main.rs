cfb00c5ff43808727ded751abd21612eb1f6b323
whoisprivalov <nikita.karatsev@gmail.com>
2021-10-14 00:44:45 +0600

    Fix folder. Add test button

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,19 @@ fn main() {
     // Or you can directly load it from a string for easy deployment.
     siv.load_toml(include_str!("assets/style.toml")).unwrap();
 
-    siv.add_layer(
-        Dialog::around(TextView::new(
+    let mut dialog1 = Dialog::around(TextView::new(
             "This application uses a \
              custom theme!",
         ))
         .title("Themed dialog")
-        .button("esc", |s| s.quit()),
-    );
+        .button("esc", |s| s.quit());
+    
+    //dialog1.buttons().set_label_raw("Quit");
+    
+    let but1 = dialog1.buttons_mut().next().unwrap();
+    but1.set_label_raw("[Quit]");
 
+    siv.add_layer(dialog1);
+    
     siv.run();
 }
